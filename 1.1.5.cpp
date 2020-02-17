6b831b0f6fa62d3ed9bc1b02e7b4e7671481f29c
MelikbekyanAshot <41271647+MelikbekyanAshot@users.noreply.github.com>
2020-02-17 19:40:48 +0300

    Update 1.1.5.cpp

--- 1.1.5.cpp
+++ 1.1.5.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << j << " ";
+    if (n > 0 && n < 10) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                cout << j << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
+    } else {
+        cout << "N is wrong: " << n;
     }
     return 0;
 }
