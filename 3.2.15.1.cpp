44cac5bd966b5c00f0d98f001d80d3d6f019bfa2
MelikbekyanAshot <41271647+MelikbekyanAshot@users.noreply.github.com>
2020-03-18 13:03:58 +0300

    Update 3.2.15.1.cpp

--- 3.2.15.1.cpp
+++ 3.2.15.1.cpp
@@ -1,48 +1,46 @@
 #include <iostream>
-#include <vector>
-#include <string>
+
 using namespace std;
 
 class MyClass1;
 
 class MyClass {
-private:
-    int property;
 public:
-    void set(MyClass &myClass, int p) {
-        myClass.property = p;
-    }
+    int property;
+	MyClass(int p) {
+		property = p;
+	}
 
-    friend void compare(MyClass &myClass, MyClass1 &myClass1);
+	friend void compare(const MyClass &myClass, const MyClass1 &myClass1);
 };
 
 class MyClass1 {
-private:
-    int property1, property2;
 public:
-    void set(MyClass1 &myClass1, int p1, int p2) {
-        myClass1.property1 = p1;
-        myClass1.property2 = p2;
-    }
+    int property1, property2;
+	MyClass1(int p1, int p2) {
+		property1 = p1;
+		property2 = p2;
+	}
 
-    friend void compare(MyClass &myClass, MyClass1 &myClass1);
+	friend void compare(const MyClass &myClass, const MyClass1 &myClass1);
 };
 
-void compare(MyClass &myClass, MyClass1 &myClass1) {
-    cout << fmax(myClass.property, fmax(myClass1.property1, myClass1.property2));
+void compare(const MyClass &myClass, const MyClass1 &myClass1) {
+
 }
 
+void compare(MyClass &myClass, MyClass1 &myClass1) {
+	cout << fmax(myClass.property , fmax(myClass1.property1, myClass1.property2));
+}
 
 int main()
 {
-    MyClass obj1{};
-    int a, b;
-    cin >> a;
-    obj1.set(obj1, a);
+    int p, p1, p2;
+    cin >> p;
+    MyClass obj1(p);
 
-    MyClass1 obj2{};
-    cin >> a >> b;
-    obj2.set(obj2, a, b);
+    cin >> p1 >> p2;
+    MyClass1 obj2(p1, p2);
 
     compare(obj1, obj2);
     return 0;
