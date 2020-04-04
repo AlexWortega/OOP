784d53ff91bb2961a214707ffd108dda6957c89e
MelikbekyanAshot <41271647+MelikbekyanAshot@users.noreply.github.com>
2020-04-04 14:33:44 +0300

    Create cl_application.h

---
+++ lab3/cl_application.h
@@ -0,0 +1,25 @@
+#ifndef CL_APPLICATION_H
+#define CL_APPLICATION_H
+
+#include "cl_3.h"
+#include "cl_4.h"
+
+//using namespace std;
+
+class cl_application : public cl_base
+{
+public:
+	cl_application ( );
+//   ~cl_application ( );
+
+	void bild_tree_objects ( );
+	int  exec_app          ( );
+
+
+private:
+	cl_base * p_ob;
+	cl_3    * p_ob_3;
+	cl_4    * p_ob_4;
+};
+
+#endif // CL_APPLICATION_H
